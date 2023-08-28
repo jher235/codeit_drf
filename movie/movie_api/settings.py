1e7f6c45649b68fbd560e3f85190628c73fd7987
jher235 <tim668666@gmail.com>
2023-08-28 14:28:44 +0900

    cors오류 해결

--- movie/movie_api/settings.py
+++ movie/movie_api/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'movies'
+    'movies',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', #위치가 중요 항상 최정상
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,6 +130,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3,
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000', #허용하고 싶은 출처 작성 ex) http://localhost:3000
+]
 
 
 
